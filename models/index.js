f57b55f66a4efb2b2db3781fbd0fa75c1c5c3001
jameswang <jameswangdr@gmail.com>
2019-09-18 16:03:15 -0700

    DB_URL = mongodb_URI

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/dashi';
+const DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/dashi';
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
